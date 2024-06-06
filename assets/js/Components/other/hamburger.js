08df3735e377c3b67165612083e8c01d85f69314
Chrstjan <ps5christian@hotmail.com>
2024-06-06 13:01:46 +0200

    Made the hamburger animation

--- assets/js/Components/other/hamburger.js
+++ assets/js/Components/other/hamburger.js
@@ -1,8 +1,28 @@
 const burgerBtn = document.getElementById("hamburger");
 const navMenu = document.querySelector(".main-nav-menu");
 
+let isOpen = false;
+
 export const toggleNavbar = () => {
     burgerBtn.addEventListener("click", () => {
+        isOpen = true
         navMenu.classList.toggle("show-menu");
+
+        document.querySelectorAll(".bar").forEach((bar, index) => {
+            switch (isOpen) {
+                case index === 0:
+                    bar.classList.toggle("burger-top");
+                    break;
+                case index === 1:
+                    bar.classList.toggle("burger-middle");
+                    break;
+                case index === 2:
+                    bar.classList.toggle("burger-bottom");
+                    break;
+                default:
+                    console.error("Index not found!");
+                    break;
+            }
+        })
     });
 }
