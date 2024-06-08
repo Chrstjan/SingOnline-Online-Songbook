db17909aa701f3b071a61a8821100f88c5b58060
Christian <ps5christian@hotmail.com>
2024-06-08 17:52:06 +0200

    Made the mobile & desktop view for artist details + plus song navigation

--- assets/js/Components/buildArtistView.js
+++ assets/js/Components/buildArtistView.js
@@ -1,68 +1,68 @@
 import { clearApp } from "./app.js";
+import { buildSongDetails } from "./buildSongDetails.js";
 
 const app = document.getElementById("app");
 const artistContainer = document.createElement("div");
 artistContainer.classList.add("artist-container");
 
 const dummyAlbumsArr = [
-    {
-        image: "./assets/img/ginger-man.jpg",
-        album_name: "Joe & the Banjos",
-        album_description: "Joe rocks the world with his banjo",
-        songs_in_track: [
-            {
-                song_name: "Summer Banjo",
-                song_name: "Shape of my banjo"
-            }
-        ]
-    },
-    {
-        image: "./assets/img/ginger-man.jpg",
-        album_name: "Joe & the Banjos",
-        album_description: "Joe rocks the world with his banjo",
-        songs_in_track: [
-            {
-                song_name: "Summer Banjo",
-                song_name: "Shape of my banjo"
-            }
-        ]
-    },
-    {
-        image: "./assets/img/ginger-man.jpg",
-        album_name: "Joe & the Banjos",
-        album_description: "Joe rocks the world with his banjo",
-        songs_in_track: [
-            {
-                song_name: "Summer Banjo",
-                song_name: "Shape of my banjo"
-            }
-        ]
-    },
-    {
-        image: "./assets/img/ginger-man.jpg",
-        album_name: "Joe & the Banjos",
-        album_description: "Joe rocks the world with his banjo",
-        songs_in_track: [
-            {
-                song_name: "Summer Banjo",
-                song_name: "Shape of my banjo"
-            }
-        ]
-    }
-]
+  {
+    image: "./assets/img/ginger-man.jpg",
+    album_name: "Joe & the Banjos",
+    album_description: "Joe rocks the world with his banjo",
+    songs_in_track: [
+      {
+        song_name: "Summer Banjo",
+        song_name: "Shape of my banjo",
+      },
+    ],
+  },
+  {
+    image: "./assets/img/ginger-man.jpg",
+    album_name: "Joe & the Banjos",
+    album_description: "Joe rocks the world with his banjo",
+    songs_in_track: [
+      {
+        song_name: "Summer Banjo",
+        song_name: "Shape of my banjo",
+      },
+    ],
+  },
+  {
+    image: "./assets/img/ginger-man.jpg",
+    album_name: "Joe & the Banjos",
+    album_description: "Joe rocks the world with his banjo",
+    songs_in_track: [
+      {
+        song_name: "Summer Banjo",
+        song_name: "Shape of my banjo",
+      },
+    ],
+  },
+  {
+    image: "./assets/img/ginger-man.jpg",
+    album_name: "Joe & the Banjos",
+    album_description: "Joe rocks the world with his banjo",
+    song_in_track: "Shape of my banjo",
+  },
+];
 
 export const buildArtistView = () => {
-    clearApp(app);
-    clearApp(artistContainer);
+  clearApp(app);
+  clearApp(artistContainer);
 
-    let artistHeader = `<header class="artist-header">
-                <h3>Artist: Banjo Joe</h3>
-            </header>`;
-    artistContainer.innerHTML += artistHeader;
+  let artistHeader = ``;
+  artistContainer.innerHTML += artistHeader;
 
-    let artistContainerContent = `
+  let artistContainerContent = `
         <figure class="artist-info-container">
+            <header class="artist-header">
+                <h3>Artist: Banjo Joe</h3>
+            </header>
             <img src="./assets/img/ginger-man.jpg" alt="Picture of the artist" />
+            <header class="desktop-artist-header">
+                <h3>Banjo Joe</h3>
+            </header>
             <figcaption>
                 <p>
                     Banjo Joe is a charismatic and talented musician known for his mastery of the banjo. 
@@ -70,23 +70,23 @@ export const buildArtistView = () => {
             </figcaption>
         </figure>`;
 
-    artistContainer.innerHTML += artistContainerContent;
+  artistContainer.innerHTML += artistContainerContent;
 
-    let artistAlbums = document.createElement("span");
-    artistAlbums.classList.add("albums-container");
+  let artistAlbums = document.createElement("span");
+  artistAlbums.classList.add("albums-container");
 
-    let albumsHeader = `
+  let albumsHeader = `
         <header class="album-header">
             <h2>Albums:</h2>
         </header>`;
 
-    artistAlbums.innerHTML += albumsHeader;
+  artistAlbums.innerHTML += albumsHeader;
 
-    dummyAlbumsArr.map((album) => {
-        let albums = `
+  dummyAlbumsArr.map((album) => {
+    let albums = `
         <figure class="albums-container-figure">
-            <img src="${album.image}" alt="Album image" />
             <header class="album-header-info">
+                <img src="${album.image}" alt="Album image" />
                 <hgroup>
                     <h3>${album.album_name}</h3>
                     <h4>${album.album_description}</h4>
@@ -94,17 +94,28 @@ export const buildArtistView = () => {
             </header>
             <figcaption>
                <header class="tracks-header">
-                <h2>Tracks:</h2>
+                <h2>Tracks: </h2>
                </header> 
-               <p>
-                ${album.songs_in_track}
-               </p>
+               <ul class="album-tracks">
+                <li><button class="album-song">Shape of my banjo</button></li>
+                <li><button class="album-song">Banjo Summer</button></li>
+                <li><button class="album-song">If you leave my banjo</button></li>
+               </ul>
             </figcaption>
-        </figure>`
+        </figure>`;
 
-        artistAlbums.innerHTML += albums;
+    artistAlbums.innerHTML += albums;
+
+    console.log(dummyAlbumsArr.songs_in_track);
+  });
+
+  artistContainer.appendChild(artistAlbums);
+  app.appendChild(artistContainer);
+
+  const songsBtn = document.querySelectorAll(".album-song");
+  songsBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      buildSongDetails();
     });
-
-    artistContainer.appendChild(artistAlbums);
-    app.appendChild(artistContainer);
-}
+  });
+};
--- assets/js/Components/buildArtists.js
+++ assets/js/Components/buildArtists.js
@@ -1,64 +1,66 @@
-import { clearApp } from '../Components/app.js';
-import { buildArtistView } from '../Components/buildArtistView.js';
+import { clearApp } from "../Components/app.js";
+import { buildArtistView } from "../Components/buildArtistView.js";
 
 const app = document.getElementById("app");
 const artistsContainer = document.createElement("div");
 artistsContainer.classList.add("artists-container");
 
 let dummyArtistsArr = [
-    {
-        image: "./assets/img/ginger-man.jpg",
-        name: "Banjo Joe"
-    },
-    {
-        image: "./assets/img/ginger-man.jpg",
-        name: "Banjo Joe"
-    },
-    {
-        image: "./assets/img/ginger-man.jpg",
-        name: "Banjo Joe"
-    },
-    {
-        image: "./assets/img/ginger-man.jpg",
-        name: "Banjo Joe"
-    },
-    {
-        image: "./assets/img/ginger-man.jpg",
-        name: "Banjo Joe"
-    },
-    {
-        image: "./assets/img/ginger-man.jpg",
-        name: "Banjo Joe"
-    }
-]
+  {
+    image: "./assets/img/ginger-man.jpg",
+    name: "Banjo Joe",
+  },
+  {
+    image: "./assets/img/ginger-man.jpg",
+    name: "Banjo Joe",
+  },
+  {
+    image: "./assets/img/ginger-man.jpg",
+    name: "Banjo Joe",
+  },
+  {
+    image: "./assets/img/ginger-man.jpg",
+    name: "Banjo Joe",
+  },
+  {
+    image: "./assets/img/ginger-man.jpg",
+    name: "Banjo Joe",
+  },
+  {
+    image: "./assets/img/ginger-man.jpg",
+    name: "Banjo Joe",
+  },
+];
 
 export const buildArtists = () => {
-    clearApp(app);
-    clearApp(artistsContainer);
+  clearApp(app);
+  clearApp(artistsContainer);
 
-    let artistsHeader = `
+  let artistsHeader = `
         <header class="artists-header">
             <h2>Artists</h2>
         </header>`;
 
-    app.innerHTML += artistsHeader;
+  app.innerHTML += artistsHeader;
 
-    dummyArtistsArr.map((artist) => {
-        let artistContainer = `
+  dummyArtistsArr.map((artist) => {
+    let artistContainer = `
             <figure class="artist-container">
                 <img src="${artist.image}"
                 <header class="artist-name">
                     <h3>${artist.name}</h3>
                 </header>
             </figure>`;
-        
-        artistsContainer.innerHTML += artistContainer;
+
+    artistsContainer.innerHTML += artistContainer;
+  });
+
+  app.appendChild(artistsContainer);
+
+  const artistFigure = document.querySelectorAll(".artist-container");
+  artistFigure.forEach((figure) => {
+    figure.addEventListener("click", () => {
+      buildArtistView();
     });
-
-    app.appendChild(artistsContainer);
-
-    const artistFigure = document.querySelector(".artist-container");
-    artistFigure.addEventListener("click", () => {
-        buildArtistView();
-    })
-}
+  });
+};
